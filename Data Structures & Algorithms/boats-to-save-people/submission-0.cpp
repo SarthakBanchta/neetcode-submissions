1aca2ceb6dfbbd6ce09672a9af966aaf9fbe026d
SarthakBanchta <sarthakbanchta17@gmail.com>
2026-07-30 12:36:40 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int l = 0;
+        int r = people.size() - 1;
+
+        int count = 0;
+        while(l <= r){
+          if(people[l] + people[r] <= limit){
+            count++;
+            l++;
+            r--;
+          }
+          else if(people[l] + people[r] > limit){
+            count++;
+            r--;
+          }
+        }
+        return count;
+        
+    }
+};
